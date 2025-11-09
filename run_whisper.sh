079dce937c11ae5023aa43b2d0b8b50f41f672be
Annika Nolte <anolte@uni-bremen.de>
2025-11-09 17:54:49 +0100

    Add run_whisper.sh

---
+++ run_whisper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+
+# First, we need to tell SLURM some information about the job
+#SBATCH --job-name=whisper
+
+# Requesting 4 GB of memory - This is just a mall example,
+# and this line allows the rest of the memory to be used
+# by other jobs
+#SBATCH --mem=4G
+
+# Estimating a runtime of at most 10 minutes - Good estimates
+# of runtime help the scheduler and may help your job get
+# scheduled earlier, but will also kill your job if it exceeds
+# the requested time
+#SBATCH --time=00:10:00
+
+# These 2 lines request a GPU
+# If no GPU is needed, you can remove them
+#SBATCH --partition=GPU
+
+# Telling SLURM where to write output and error files
+# %j gives job id
+# %x gives job name
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+
+
+
+# The actual code to be run starts here
+
+# Load the python LMod module so we can use the latest version
+# Check available modules with `module avail`
+module load python
+
+# Activate the virtual environment to get access to the required packages
+source whisper-venv/bin/activate
+
+# Run the main script to generate predictions
+python3 whisperx.py
+
+# Deactivate the virtual environment
+deactivate
